f9c9608829e8a52338e8b10685e1ff90057dae39
fluzzyy <gunte95@hotmail.com>
2019-04-11 09:18:10 +0200

    Initial Commit

---
+++ mindMapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mindMapApp
+//
+//  Created by Alexander Gunnarsson on 2019-04-11.
+//  Copyright © 2019 Alexander Gunnarsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
